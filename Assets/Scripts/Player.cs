2f20217d39d23e6fca4ecdccede5e2094392559c
YoungSi4 <dksdudgns01@gmail.com>
2024-07-02 17:27:25 +0900

    YH - player move on Task (still not move)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-
+    // 플레이어 변수
+    // 모두 { get; }을 달아서 읽기 전용으로 변경
     
-    // Start is called before the first frame update
-    void Start()
+    // 플레이어 스탯
+    [Header("- Player")]
+    public float playerMaxHp;
+    public float playerCurHp { get; }
+    public float playerAtk { get; }
+    public float playerAtkSpeed { get; }
+    public float playerMoveSpeed;
+    public float playerCritPer { get; }
+    public float playerCritDmg { get; }
+    private float playerInvincibleTime;
+
+    // 플레이어 상태 (방향 등)
+    private Vector2 playerMoveVector;
+    private Vector2 playerDir;
+
+    private void Update()
     {
-        
+        var player2DDir = playerMoveVector;
+        player2DDir.Normalize();
+        transform.Translate(player2DDir * playerMoveSpeed * Time.deltaTime, Space.World);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void playerMove(InputAction.CallbackContext context)
     {
-        
+        if (context.started)
+        {
+            playerMoveVector = context.ReadValue<Vector2>();
+        }
+
+        if (context.canceled)
+        {
+            playerMoveVector = Vector2.zero;
+        }
     }
+
 }
