1069f057200f7c530a298a050d58c921c81abd37
YoungSi4 <dksdudgns01@gmail.com>
2024-07-03 16:57:39 +0900

    YH - Sprint and Invincible Developed !

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float damage = 2f;
+    // bullet stats / initialize needed
+    public float bulletDamage = 2f;
+    public float bulletSpeed = 5f;
+    
+    // direction
+    public Vector2 bulletVector = Vector2.up;
 
+    // delete
+    private float timeCounter = 0f;
+    private const float bulletMaxTime = 5f;
+
+    // Init , Start
+
+    private void Update()
+    {
+        // bullet move
+        transform.Translate(bulletVector * (bulletSpeed * Time.deltaTime));
+
+        timeCounter += Time.deltaTime;
+        if (timeCounter > bulletMaxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    // Damage - fixflag - YH still on Task
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            collision.GetComponent<MeleeEnemy>().TakeDamage(damage);
+            collision.GetComponent<MeleeEnemy>().TakeDamage(bulletDamage);
             Destroy(gameObject);
         }
     }
