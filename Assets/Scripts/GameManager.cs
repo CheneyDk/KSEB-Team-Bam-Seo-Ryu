983d149ebb8b8f8554cba68ec9066fbdfff449bd
YoungSi4 <dksdudgns01@gmail.com>
2024-07-04 15:41:18 +0900

    YH - gameover, scene reload Developed !

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -8,21 +10,33 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     // UI
     [Header("- UI")]
-    public GameObject GameoverCanvas;
+    public GameObject GameoverUI;
+    // esc menua
+
+    [Header("- System")]
+    // public GameObject WaveManager;
 
     // flags
-    private bool isPlayerAlive = true;
+    private bool isPlayerAlive;
 
 
     private void Awake()
     {
         Instance = this; // singleton?
-        GameoverCanvas.SetActive(false);
+        isPlayerAlive = true;
+        GameoverUI.SetActive(false);
     }
 
     public void SetPlayerDead()
     {
         isPlayerAlive = false;
-        GameoverCanvas.SetActive(true);
+        GameoverUI.SetActive(true);
+        // WaveManager.SetActive(false); // YH - activate after merge
     }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
