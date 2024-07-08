5afd3c6d3b2e8c8af5d6ca260c3033b47e4f1de9
CheneyDk <adt12micen@gmail.com>
2024-07-08 16:03:43 +0900

    Game UI에 exp구현. player에서 함수들이 없어서error발생중

--- Assets/Scripts/UI/Game UI.cs
+++ Assets/Scripts/UI/Game UI.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
+    [Header("Wave")]
     [SerializeField]
     private TextMeshProUGUI waveNumber;
     [SerializeField]
     private TextMeshProUGUI waveTimer;
+
+    [Header("Bar")]
     [SerializeField]
-    private Slider healBar;
+    private Slider healthBar;
+    [SerializeField]
+    private TextMeshProUGUI healthText;
+    [SerializeField]
+    private Slider expBar;
+    [SerializeField]
+    private TextMeshProUGUI expText;
 
     [SerializeField]
     private WaveManager waveManager;
@@ -23,13 +32,16 @@ public class GameUI : MonoBehaviour
     {
         waveNumber.text = "WAVE : " + waveManager.curWave.ToString();
         waveTimer.text = waveManager.time.ToString("N0");
-        healBar.value = healBar.maxValue;
+        healthText.text = player.playerCurHp.ToString("N0") + " / " + player.playerMaxHp.ToString("N0");
+        expText.text = player.playerCurExp.ToString("N0") + " / " + player.playerMaxExp.ToString("N0");
+        healthBar.value = healthBar.maxValue;
     }
 
     private void Update()
     {
         waveNumber.text = "WAVE : " + waveManager.curWave.ToString();
         waveTimer.text = waveManager.time.ToString("N0");
-        healBar.value = healBar.maxValue * (player.playerCurHp / player.playerMaxHp);
+        healthBar.value = healthBar.maxValue * (player.playerCurHp / player.playerMaxHp);
+        expBar.value = expBar.maxValue * (player.playerCurExp / player.playerMaxExp);
     }
 }
