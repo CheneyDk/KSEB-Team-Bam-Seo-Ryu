4e21fc2f2a5ec35818e070e0dbcd6dc8f52c2466
YoungSi4 <dksdudgns01@gmail.com>
2024-07-09 11:04:59 +0900

    YH - magetic Developed !

--- Assets/Scripts/Items Scripts/Item.cs
+++ Assets/Scripts/Items Scripts/Item.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
+    // item values
     protected float value;
+    
+    // magnetic system
+    private float itemMagneticRange;
+    private float itemMagneticMoveSpeed = 10f;
+
+    private Transform playerTrans;
+
+    private void Start()
+    {
+        // Init
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        itemMagneticRange = 5f; // YH - test
+    }
+
+    private void Update()
+    {
+        ItemMagnetic();
+    }
+
 
     // collision with player
     protected abstract void OnTriggerEnter2D(Collider2D collision);
@@ -12,4 +32,22 @@ public abstract class Item : MonoBehaviour
     public abstract void Init(float expAmount);
     // Hp potion and Red Bool have their' own const value.
     // Init on Awake and make this func null return or something.
+
+
+    public void SetMagneticRange(float range)
+    {
+        itemMagneticRange = range;
+    }
+
+
+    private void ItemMagnetic()
+    {
+        // distance calculate
+        var distance = Vector3.Distance(transform.position, playerTrans.position);
+
+        if (distance < itemMagneticRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTrans.position, itemMagneticMoveSpeed * Time.deltaTime);
+        } 
+    }
 }
