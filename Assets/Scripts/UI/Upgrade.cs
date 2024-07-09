8159410bd4b750d529b42dd3026f96b3ba1525d2
CheneyDk <adt12micen@gmail.com>
2024-07-09 11:17:13 +0900

    enemy & player 2D 그림 넣기 + upgrade script만들기

---
+++ Assets/Scripts/UI/Upgrade.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Upgrade : MonoBehaviour
+{
+    public GameObject levelUpUI;
+    public Button[] optionButtons;
+    public TextMeshProUGUI[] optionTexts;
+
+    private List<string> items = new List<string> { "a", "b", "c", "d", "e", "f" };
+
+    private void Start()
+    {
+        //levelUpUI.SetActive(false);
+        OnLevelUp();
+    }
+
+    public void OnLevelUp()
+    {
+        levelUpUI.SetActive(true);
+
+        List<string> selectedItems = new List<string>();
+        while (selectedItems.Count < 3)
+        {
+            string item = items[Random.Range(0, items.Count)];
+            if (!selectedItems.Contains(item))
+            {
+                selectedItems.Add(item);
+            }
+        }
+
+        for (int i = 0; i < optionButtons.Length; i++)
+        {
+            optionTexts[i].text = selectedItems[i];
+            optionButtons[i].onClick.RemoveAllListeners();
+            string selectedItem = selectedItems[i];
+            optionButtons[i].onClick.AddListener(() => OnItemSelected(selectedItem));
+        }
+    }
+
+    private void OnItemSelected(string item)
+    {
+        Debug.Log($"{item} 아이템 선택.");
+
+        levelUpUI.SetActive(false);
+    }
+}
