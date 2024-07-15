05b642810e5e28e49d6bf6696976f108522b6cd6
udongdong <kimdongkyun0411@gmail.com>
2024-07-15 17:33:45 +0900

    UDD - Mainmenu
    Welcome Screen 생성 및 Go버튼, 스크립트 생성

--- Assets/Scripts/Player Script/Weapon/WeaponUroboros_L.cs
+++ Assets/Scripts/Player Script/Weapon/WeaponUroboros_L.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.PlayerSettings;
 
 public class WeaponUroboros_L : PlayerWeapon
 {
