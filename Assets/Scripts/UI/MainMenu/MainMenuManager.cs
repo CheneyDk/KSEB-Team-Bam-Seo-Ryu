25edea1295fdb9dbf8388302c953cc6777a903d8
udongdong <kimdongkyun0411@gmail.com>
2024-07-18 09:47:00 +0900

    UDD - Mainmenu, Script
    사운드 관련 스크립트 MainmenuManager에 통합

--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -4,6 +4,8 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.UI.Image;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -11,6 +13,14 @@ public class MainMenuManager : MonoBehaviour
     
     public GameObject windowPanel;
     public GameObject firstPagePanel;
+    public Slider SoundSlider;
+    public GameObject I_100;
+    public GameObject I_66;
+    public GameObject I_33;
+    public GameObject I_0;
+    public GameObject I_Except;
+
+    private float Origin = 100;
 
     void Awake()
     {
@@ -51,6 +61,74 @@ public class MainMenuManager : MonoBehaviour
     public void MoveScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeIcon(float value)
+    {
+        if (value >= 66)
+        {
+            I_100.SetActive(true);
+            I_66.SetActive(false);
+            I_33.SetActive(false);
+            I_0.SetActive(false);
+            I_Except.SetActive(false);
+        }
+        else if (value >= 33)
+        {
+            I_100.SetActive(false);
+            I_66.SetActive(true);
+            I_33.SetActive(false);
+            I_0.SetActive(false);
+            I_Except.SetActive(false);
+        }
+        else if (value > 0)
+        {
+            I_100.SetActive(false);
+            I_66.SetActive(false);
+            I_33.SetActive(true);
+            I_0.SetActive(false);
+            I_Except.SetActive(false);
+        }
+        else if (value == 0)
+        {
+            I_100.SetActive(false);
+            I_66.SetActive(false);
+            I_33.SetActive(false);
+            I_0.SetActive(true);
+        }
+    }
+
+    public void Mute()
+    {
+        if (SoundSlider.value == 0)
+        {
+            if (I_0.activeSelf)
+            {
+                I_0.SetActive(false);
+                I_Except.SetActive(true);
+            }
+            else
+            {
+                I_0.SetActive(true);
+                I_Except.SetActive(false);
+            }
+        }
+        else
+        {
+            if (I_0.activeSelf)
+            {
+                SoundSlider.value = Origin - 1;
+                SoundSlider.value++;
+            }
+            else
+            {
+                Origin = SoundSlider.value;
+                I_0.SetActive(true);
+                I_100.SetActive(false);
+                I_66.SetActive(false);
+                I_33.SetActive(false);
+            }
+        }
     }
 
     public void QuitGame()
